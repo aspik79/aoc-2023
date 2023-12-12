7f134c3d2514b10a68fe8ffcdb8b8de99fabdd9b
Christoph Jobmann <christoph.jobmann@deutschebahn.com>
2023-12-12 07:32:40 +0100

    boilerplate

--- p19/src/main.rs
+++ p19/src/main.rs
@@ -1,3 +1,18 @@
+//use std::cmp;
+use std::env;
+use std::fs;
+//use std::collections::HashMap;
+//use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let items = contents.lines();
 }
